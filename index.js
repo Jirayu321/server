54180c804f0c5a3055128d2d387d5a827ac5543f
Jirayu Yung <50855666+Jirayu321@users.noreply.github.com>
2023-10-15 17:33:13 +0700

    update code 01

--- index.js
+++ index.js
@@ -37,8 +37,9 @@ app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-    "Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET"
+
   );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
   // if (req.method === "OPTIONS") {
   //   res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
   //   return res.status(200).json({});
